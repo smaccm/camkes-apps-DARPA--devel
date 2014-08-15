e314439b572bcf0327531050538ccb272286b861
Siwei Zhuang <siwei.zhuang@nicta.com.au>
2014-08-15 15:15:14 +1000

    Fix: GPIO interface has been changed.

--- components/gpio/src/drv.c
+++ components/gpio/src/drv.c
@@ -15,40 +15,40 @@
 
 #include "gpio.h"
 
-#define XEINT0  GPX0, 0
-#define XEINT1  GPX0, 1
-#define XEINT2  GPX0, 2
-#define XEINT3  GPX0, 3
-#define XEINT4  GPX0, 4
-#define XEINT5  GPX0, 5
-#define XEINT6  GPX0, 6
-#define XEINT7  GPX0, 7
-#define XEINT8  GPX1, 0
-#define XEINT9  GPX1, 1
-#define XEINT10 GPX1, 2
-#define XEINT11 GPX1, 3
-#define XEINT12 GPX1, 4
-#define XEINT13 GPX1, 5
-#define XEINT14 GPX1, 6
-#define XEINT15 GPX1, 7
-#define XEINT16 GPX2, 0
-#define XEINT17 GPX2, 1
-#define XEINT18 GPX2, 2
-#define XEINT19 GPX2, 3
-#define XEINT20 GPX2, 4
-#define XEINT21 GPX2, 5
-#define XEINT22 GPX2, 6
-#define XEINT23 GPX2, 7
-#define XEINT24 GPX3, 0
-#define XEINT25 GPX3, 1
-#define XEINT26 GPX3, 2
-#define XEINT27 GPX3, 3
-#define XEINT28 GPX3, 4
-#define XEINT29 GPX3, 5
-#define XEINT30 GPX3, 6
-#define XEINT31 GPX3, 7
-#define UART0_CTSN  GPA0, 2
-#define UART0_RTSN  GPA0, 3
+#define XEINT0  GPIOID(GPX0, 0)
+#define XEINT1  GPIOID(GPX0, 1)
+#define XEINT2  GPIOID(GPX0, 2)
+#define XEINT3  GPIOID(GPX0, 3)
+#define XEINT4  GPIOID(GPX0, 4)
+#define XEINT5  GPIOID(GPX0, 5)
+#define XEINT6  GPIOID(GPX0, 6)
+#define XEINT7  GPIOID(GPX0, 7)
+#define XEINT8  GPIOID(GPX1, 0)
+#define XEINT9  GPIOID(GPX1, 1)
+#define XEINT10 GPIOID(GPX1, 2)
+#define XEINT11 GPIOID(GPX1, 3)
+#define XEINT12 GPIOID(GPX1, 4)
+#define XEINT13 GPIOID(GPX1, 5)
+#define XEINT14 GPIOID(GPX1, 6)
+#define XEINT15 GPIOID(GPX1, 7)
+#define XEINT16 GPIOID(GPX2, 0)
+#define XEINT17 GPIOID(GPX2, 1)
+#define XEINT18 GPIOID(GPX2, 2)
+#define XEINT19 GPIOID(GPX2, 3)
+#define XEINT20 GPIOID(GPX2, 4)
+#define XEINT21 GPIOID(GPX2, 5)
+#define XEINT22 GPIOID(GPX2, 6)
+#define XEINT23 GPIOID(GPX2, 7)
+#define XEINT24 GPIOID(GPX3, 0)
+#define XEINT25 GPIOID(GPX3, 1)
+#define XEINT26 GPIOID(GPX3, 2)
+#define XEINT27 GPIOID(GPX3, 3)
+#define XEINT28 GPIOID(GPX3, 4)
+#define XEINT29 GPIOID(GPX3, 5)
+#define XEINT30 GPIOID(GPX3, 6)
+#define XEINT31 GPIOID(GPX3, 7)
+#define UART0_CTSN  GPIOID(GPA0, 2)
+#define UART0_RTSN  GPIOID(GPA0, 3)
 #define CAN_CSn          XEINT16
 #define CAN_INTn         XEINT15
 #define CAN_RESETn       XEINT25
@@ -117,7 +117,7 @@ int gpio_pinmux_config(int peripheral, int flags)
 int run(void)
 {
 	gpio_t gpio_int;
-	gpio_new(&gpio_sys, GPX1, 7, GPIO_DIR_IN, &gpio_int);
+	gpio_new(&gpio_sys, CAN_INTn, GPIO_DIR_IN, &gpio_int);
 
 	while (1) {
 		if (!gpio_get(&gpio_int)) {
