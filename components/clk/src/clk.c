9c72237540fec77e88a03a42979dc0ff9db63fa3
akroh <Alexander.Kroh@nicta.com.au>
2014-09-01 17:33:38 +1000

    Support trivial SPI speed control. Input clock is 24MHz (XXTI) so resolution will be terrible at high speed.

--- components/clk/src/clk.c
+++ components/clk/src/clk.c
@@ -18,9 +18,25 @@ clock_sys_t clock_sys;
 static int set_spi_clock(int periph_id, unsigned long rate)
 {
     volatile uint32_t *base = (uint32_t*)cmu_top_clk;
-    /* FIXME */
-    base[0x55C/4] = 0xFFFF0000;
-    return rate;
+    uint32_t r, rpre, v;
+    int fin = 24000000; /* XXTI */
+    int div = fin / rate;
+
+    /* Tune PRE_RATIO with RATIO at maximum */
+    rpre = (div / 0xf);
+    /* Now tune RATIO */
+    r = div / (rpre + 1);
+
+    /* Apply the change */
+    v = base[0x55C/4];
+    v &= 0xffff;
+    v |= rpre << 24 | r << 16;
+    base[0x55C/4] = v; 
+
+    /* Read it back */
+    rpre = (v >> 24) & 0xf;
+    r = (v >> 16) & 0xf;
+    return fin / (rpre + 1) / (r + 1);
 }
 
 unsigned int clktree_get_spi1_freq(void){
